0be90c8d77b15b5524aea3117985bf14032f9039
st0waway <lumixbd@gmail.com>
2023-02-17 12:55:37 +0200

    Add temp files

---
+++ Assignment-6/Assignment-6/Two.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6
+{
+	internal class Two
+	{
+		public static int find_it(int[] seq)
+		{
+			return seq.Where(e => e % 2 != 0).Count();
+		}
+
+
+	}
+}
+}
