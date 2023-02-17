01dc96e839d77c135d0bebb80a1e0c174e3dc7a5
st0waway <lumixbd@gmail.com>
2023-02-17 13:27:16 +0200

    Add angle substraction

--- Assignment-07/Assignment-07/Angle.cs
+++ Assignment-07/Assignment-07/Angle.cs
@@ -39,6 +39,39 @@
 			return new Angle(degrees, minutes, seconds);
 		}
 
+		public static Angle operator -(Angle angle1, Angle angle2)
+		{
+			var seconds = 0;
+			var minutes = 0;
+			var degrees = 0;
+
+			if (angle1.Seconds >= angle2.Seconds)
+			{
+				seconds = angle1.Seconds - angle2.Seconds;
+			}
+
+			else
+			{
+				seconds = angle1.Seconds + 60 - angle2.Seconds;
+				angle1.Minutes--;
+			}
+
+			if (angle1.Minutes >= angle2.Minutes)
+			{
+				minutes = angle1.Minutes - angle2.Minutes;
+			}
+
+			else
+			{
+				minutes = angle1.Minutes + 60  - angle2.Minutes;
+				angle1.Degree--;
+			}
+
+			degrees = angle1.Degree - angle2.Degree;
+
+			return new Angle(degrees, minutes, seconds);
+		}
+
 		public Angle(int degree, int minutes, int seconds)
 		{
 			this.Degree = degree;
--- Assignment-07/Assignment-07/Program.cs
+++ Assignment-07/Assignment-07/Program.cs
@@ -10,7 +10,11 @@ namespace Assignment_07
 			var angleTwo = new Angle(4, 27, 45);
 
 			var angleThree = angleOne + angleTwo;
+			var angleFour = angleTwo - angleOne;
+			var angleFive = angleOne - angleOne;
 			Console.WriteLine($"{angleThree.Degree}, {angleThree.Minutes}, {angleThree.Seconds}");
+			Console.WriteLine($"{angleFour.Degree}, {angleFour.Minutes}, {angleFour.Seconds}");
+			Console.WriteLine($"{angleFive.Degree}, {angleFive.Minutes}, {angleFive.Seconds}");
 		}
 	}
 }
