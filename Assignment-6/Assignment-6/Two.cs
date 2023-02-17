3aebab0d5272cea571866195b86f45a1dcbc94a8
st0waway <lumixbd@gmail.com>
2023-02-17 14:54:04 +0200

    Clean class two

--- Assignment-6/Assignment-6/Two.cs
+++ Assignment-6/Assignment-6/Two.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assignment_6
+﻿namespace Assignment_6
 {
 	internal class Two
 	{
-		public static int find_it(int[] seq)
-		{
-			return seq.Where(e => e % 2 != 0).Count();
-		}
-
 
 	}
-}
 }
